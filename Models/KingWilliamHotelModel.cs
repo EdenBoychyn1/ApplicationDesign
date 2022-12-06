98455685656678467e6f30b546baad3a3b88bfd6
EdenBoychyn1 <eden.boychyn@dcmail.ca>
2022-12-06 15:59:13 -0500

    Fixed MakeAReservation Form

--- Models/KingWilliamHotelModel.cs
+++ Models/KingWilliamHotelModel.cs
@@ -136,17 +136,7 @@ namespace ApplicationDesign.Models
             return loginValid;
         }
 
-        //public string Hash(string password)
-        //{
-        //    var bytes = new UTF8Encoding().GetBytes(password);
-        //    byte[] hashBytes;
-        //    using (var algorithm = new System.Security.Cryptography.SHA512Managed())
-        //    {
-        //        hashBytes = algorithm.ComputeHash(bytes);
-        //    }
-        //    string hashed_password = Convert.ToBase64String(hashBytes);
-        //    return hashed_password;
-        //}
+
     }
     #endregion
 }
